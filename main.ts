f597d2d084543b7364670cbf661da392b2b42f60
agnotario <agnotario@gmail.com>
2024-09-25 13:06:02 +0200

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@
  * Provides access to basic micro:bit functionality.
  */
 
-//% color=190 weight=100 icon="\f086" block="Basic Blocks"
+//% color=190 weight=100 icon="\uf086" block="LLM Chat"
 namespace LLMChat {
-    
+    //%blockId=SendToAPI
     //% block="Send data to API: $data"
     export function sendToAPI(data: string): void {
         // Aquí colocarás la lógica para enviar datos
